5f1bc0730b2d27d4dd39fe8e8063138e2818ef2c
bnethanel <bnethanel@gmail.com>
2025-04-22 15:41:00 +0300

    created server and server- service logic and created data

--- server.js
+++ server.js
@@ -1,4 +1,56 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.get('/', (req, res) => res.send('Hello there!!'))
+
+const port = 3030
+
+app.listen(port, () => console.log(`Server ready at http://127.0.0.1:${port}`))
+
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(400).send('Cannot load bugs')
+        })
+})
+
+
+app.get('/api/bug/save', (req, res) => {
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        severity: req.query.severity
+    }
+    bugService.save(bugToSave)
+        .then(savedBug => res.send(savedBug))
+        .catch(err => {
+            loggerService.error('Cannot save bug', err)
+            res.status(400).send('Cannot save bug')
+        })
+})
+
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug', err)
+            res.status(400).send('Cannot get bug')
+        })
+})
+
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(`Bug removed - ${bugId}`))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(400).send('Cannot remove bug')
+        })
+ })
